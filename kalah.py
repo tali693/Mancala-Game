66583928bf48f2e73701a99b2bd04ff65a9291fb
Tali <tali693@gmail.com>
2018-11-19 14:18:45 +0200

    1.1 Initial Status done

--- kalah.py
+++ kalah.py
@@ -1,2 +1,10 @@
 class Kalah(object):
-    pass
+    def __init__(self, holes, seeds):
+        self.holes = holes
+        self.seeds = seeds
+        self.board = [seeds for h in range(holes)]
+        self.board.append(0)
+        self.board = self.board * 2
+
+    def get_board(self):
+        return self.board
--- test_kalah.py
+++ test_kalah.py
@@ -1,8 +1,15 @@
+from kalah import Kalah
 import unittest
 
 
 class KalahTestCase(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.game = Kalah(6, 4)
+
+    def test_1_1_initial_status(self):
+
+        self.assertEqual(self.game.get_board(), [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0])
 
 
 if __name__ == '__main__':
